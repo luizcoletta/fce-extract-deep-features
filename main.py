dfe00e58f2ebe28dbe19b04571b85a136d8b9ab1
Douglas Cardozo de Almeida <douglas.almeida@unesp.br>
2021-08-06 18:27:42 -0300

    Changes to cut the image with data from arff and save the sub images in new folders

--- main.py
+++ main.py
@@ -6,42 +6,51 @@ import os
 # https://stackoverflow.com/questions/3430372/how-do-i-get-the-full-path-of-the-current-files-directory
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
+# Retrieve the dicc from file arff
 data_file_name = "ceratocystis_test_90_10_rgb_11_fs9_res1-Indexes.arff"
 data_path = curr_dir + "/data/" + data_file_name
-
 data = arff.load(open(data_path))
 
 # Convert dicc to list. Only the data
 lst_data = list(data.values())[3]
 
-# Loop to search one register of listNoCoord in listWithCoord
-'''for l in lst_data:
-    first_or_default = next((x for x in listWithCoord if x[3] == l[0] and x[4] == l[1] and x[5] == l[2]), None)
-    if first_or_default is not None:
-        #list to be appended to the listMatched. Contains the number of image, coord x, coord y, real class, predicted class
-        abc = [first_or_default[0], first_or_default[1], first_or_default[2], int(first_or_default[-1]), int(l[-1])]
-        listMatched.append(abc)
+# Convert list to numeric int array
+lst_data = np.array(lst_data)
+num_data = lst_data.astype(float)
+num_data = np.around(num_data)
+num_data = num_data.astype(int)
 
-#Write of a .txt file with the listMatched
-with open('imageCoordTo'+ResultFile+'.txt', 'w') as f:
-    for item in listMatched:
-        f.write("%4.0f\t%4.0f\t%4.0f\t%s\t%s\n" % (item[0], item[1], item[2], item[3], item[4]))'''
+# Get dir name from file arff
+dirDataName = data_file_name[data_file_name.index("res"):data_file_name.index("-")]
+# Load imgs
+img = cv2.imread('images/IMG_11.JPG')
+imgLabeled = cv2.imread('images/IMG_11_Labeled.png')
 
+for l in num_data:
 
+    # Get name for imgs cut
+    imgName = f'{dirDataName}_{l[1]}_{l[2]}_{l[-1]}'
 
-# https://learnopencv.com/cropping-an-image-using-opencv/
-img = cv2.imread('images/test.png')
-print(img.shape)  # Print image shape
-cv2.imshow("original", img)
+    # Img not labeled
+    cropped_img = img[l[1]:l[1] + 10, l[2]:l[2] + 10]  # img[start_row:end_row, start_col:end_col]
+    # Create folder, if not exists
+    dirname = 'results/' + dirDataName + '/notLabeled'
+    if os.path.isdir(dirname):
+        pass
+    else:
+        os.makedirs(dirname)
+    # Save img not labeled
+    cv2.imwrite(os.path.join(dirname, str(imgName) + '.png'), cropped_img)
+    cv2.waitKey(0)
 
-# Cropping an image
-cropped_image = img[80:280, 150:330]  # img[start_row:end_row, start_col:end_col]
-
-# Display cropped image
-cv2.imshow("cropped", cropped_image)
-
-# Save the cropped image
-cv2.imwrite("Cropped Image.png", cropped_image)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Img labeled
+    cropped_imgLabeled = imgLabeled[l[1]:l[1] + 10, l[2]:l[2] + 10]  # img[start_row:end_row, start_col:end_col]
+    # Create folder, if not exists
+    dirname = 'results/' + dirDataName + '/Labeled'
+    if os.path.isdir(dirname):
+        pass
+    else:
+        os.makedirs(dirname)
+    # Save img labeled
+    cv2.imwrite(os.path.join(dirname, str(imgName) + '.png'), cropped_imgLabeled)
+    cv2.waitKey(0)
